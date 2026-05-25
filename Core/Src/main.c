600654c750bf60dbeb2bd1df82b81027cdbc8d3f
anas <mansouri.anas.98@gmail.com>
2026-05-25 22:11:28 +0200

    add ADC load telemetry

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -38,6 +38,7 @@ typedef struct
 {
 	int temperature;
 	int humidity;
+	int load;
 } TelemetryData;
 /* USER CODE END PTD */
 
@@ -52,6 +53,8 @@ typedef struct
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
@@ -87,7 +90,8 @@ const osThreadAttr_t TelemetryTask_attributes = {
 osMessageQueueId_t commandQueueHandle;
 TelemetryData latestTelemetry = {
     .temperature = 19,
-    .humidity = 60
+    .humidity = 60,
+	.load =0
 };
 /* USER CODE END PV */
 
@@ -96,6 +100,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_ADC1_Init(void);
 void StartDefaultTask(void *argument);
 void StartUartRxTask(void *argument);
 void StartCommandTask(void *argument);
@@ -141,6 +146,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_USART1_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -299,6 +305,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -531,7 +589,7 @@ void StartCommandTask(void *argument)
     else if (strcmp(cmd.text, "GET_STATUS") == 0)
     {
       char statusMsg[50];
-      snprintf(statusMsg,sizeof(statusMsg),"STATUS:TEMP=%d;HUM=%d\r\n",latestTelemetry.temperature,latestTelemetry.humidity);
+      snprintf(statusMsg,sizeof(statusMsg),"STATUS:TEMP=%d;HUM=%d;LOAD=%d\r\n",latestTelemetry.temperature,latestTelemetry.humidity,latestTelemetry.load);
       HAL_UART_Transmit(&huart1, (uint8_t *)statusMsg, strlen(statusMsg), HAL_MAX_DELAY);
     }
     else
@@ -556,6 +614,20 @@ void StartTelemetryTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
+
+	  HAL_ADC_Start(&hadc1);
+
+	  if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
+	  {
+		  uint32_t adcRaw = HAL_ADC_GetValue(&hadc1);
+
+	      latestTelemetry.load = (adcRaw * 100) / 4095;
+	  }
+
+	  HAL_ADC_Stop(&hadc1);
+
+
+
 	  latestTelemetry.humidity++;
 	  latestTelemetry.temperature++;
       if(latestTelemetry.humidity>=100){
